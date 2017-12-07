e111c0e833f5e3892904bc9eda6e18a17e48d1e2
Zoyix <851565833@qq.com>
2017-12-07 17:00:09 +0800

    编写自己的元注解和annotationProcessor8-1

---
+++ latte-compiler/src/main/java/com/dianbin/latte/compiler/LatteProcessor.java
@@ -0,0 +1,76 @@
+package com.dianbin.latte.compiler;
+
+import com.dianbin.latte.annotations.AppRegisterGenerator;
+import com.dianbin.latte.annotations.EntryGenerator;
+import com.dianbin.latte.annotations.PayEntryGenerator;
+import com.google.auto.service.AutoService;
+
+import java.lang.annotation.Annotation;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.AnnotationValueVisitor;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+
+/**
+ * Created by Administrator on 2017/12/7.
+ */
+
+@SuppressWarnings("unused")
+@AutoService(Processor.class)  //TODO 为什么要用这个注解生成meta信息，meta信息有什么用？
+public class LatteProcessor extends AbstractProcessor {
+
+    //TODO 将注解过的类的类型传进来有什么用？ 调试下传进来的类型是啥？
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        final Set<String> types = new LinkedHashSet<>();
+        final Set<Class<? extends Annotation>> supportAnnotations = getSupportedAnnotations();
+        for (Class<? extends Annotation> annotation : supportAnnotations) {
+            types.add(annotation.getCanonicalName());
+        }
+        return types;
+    }
+
+    private Set<Class<? extends Annotation>> getSupportedAnnotations() {
+        //TODO LinkedHashSet这个类?
+        final Set<Class<? extends Annotation>> annotations = new LinkedHashSet<>();
+        annotations.add(EntryGenerator.class);
+        annotations.add(PayEntryGenerator.class);
+        annotations.add(AppRegisterGenerator.class);
+        return annotations;
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+        return false;
+    }
+
+    //TODO 这个方法看不懂？有啥用？
+    //TODO AnnotationValueVisitor是什么？有什么用？
+    private void scan(RoundEnvironment env, Class<? extends Annotation> annotation, AnnotationValueVisitor visitor) {
+        for (Element typeElement : env.getElementsAnnotatedWith(annotation)) {
+            final List<? extends AnnotationMirror> annotationMirrors = typeElement.getAnnotationMirrors();
+
+            for (AnnotationMirror annotationMirror : annotationMirrors) {
+                final Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = annotationMirror.getElementValues();
+
+                for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : elementValues.entrySet()){
+                    entry.getValue().accept(visitor,null);
+                }
+            }
+        }
+    }
+
+    private void generateEntryCode(RoundEnvironment env){
+        
+    }
+}
