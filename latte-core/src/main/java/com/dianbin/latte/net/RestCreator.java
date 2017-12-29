356c5b69a7e8a2ccdedb899e54e55b74d784fe02
Zoyix <851565833@qq.com>
2017-12-29 13:47:41 +0800

    完成5-11（上次漏下的）

--- latte-core/src/main/java/com/dianbin/latte/net/RestCreator.java
+++ latte-core/src/main/java/com/dianbin/latte/net/RestCreator.java
@@ -2,6 +2,7 @@ package com.dianbin.latte.net;
 
 import com.dianbin.latte.app.ConfigKeys;
 import com.dianbin.latte.app.Latte;
+import com.dianbin.latte.net.rx.RxRestServise;
 
 import java.util.ArrayList;
 import java.util.WeakHashMap;
@@ -10,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 /**
@@ -27,9 +29,6 @@ public class RestCreator {
         return ParamsHolder.PARAMS;
     }
 
-    public static RestServise getRestService() {
-        return RestServiceHolder.REST_SERVISE;
-    }
 
     private static final class RetrofitHolder {
         private static final String BASE_URL = (String) Latte.getConfigurations().get(ConfigKeys.API_HOST);
@@ -37,6 +36,7 @@ public class RestCreator {
                 .baseUrl(BASE_URL)
                 .client(OKHttpHolder.OK_HTTP_CLIENT)
                 .addConverterFactory(ScalarsConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
     }
 
@@ -59,9 +59,26 @@ public class RestCreator {
                 .build();
     }
 
+    /**
+     * Service接口
+     */
     private static final class RestServiceHolder {
         private static final RestServise REST_SERVISE = RetrofitHolder.RETROFIT_CLIENT.create(RestServise.class);
     }
 
+    public static RestServise getRestService() {
+        return RestServiceHolder.REST_SERVISE;
+    }
+
+    /**
+     * RxService接口
+     */
+    private static final class RxRestServiceHolder {
+        private static final RxRestServise REST_SERVISE = RetrofitHolder.RETROFIT_CLIENT.create(RxRestServise.class);
+    }
+
+    public static RxRestServise getRxRestService() {
+        return RxRestServiceHolder.REST_SERVISE;
+    }
 
 }
