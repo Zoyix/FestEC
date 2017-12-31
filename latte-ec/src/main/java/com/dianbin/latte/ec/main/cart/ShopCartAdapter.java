9e457209b2ab9e34a5573add4504811da45e66d6
Zoyix <Zoyixwork@163.com>
2017-12-31 22:22:58 +0800

    13-4

--- latte-ec/src/main/java/com/dianbin/latte/ec/main/cart/ShopCartAdapter.java
+++ latte-ec/src/main/java/com/dianbin/latte/ec/main/cart/ShopCartAdapter.java
@@ -1,5 +1,7 @@
 package com.dianbin.latte.ec.main.cart;
 
+import android.graphics.Color;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.View;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
+import com.dianbin.latte.app.Latte;
 import com.dianbin.latte.ec.R;
 import com.dianbin.latte.ui.recycle.MultipleFields;
 import com.dianbin.latte.ui.recycle.MultipleItemEntity;
@@ -37,7 +40,7 @@ public class ShopCartAdapter extends MultipleRecyclerAdapter {
     }
 
     @Override
-    protected void convert(MultipleViewHolder holder, MultipleItemEntity entity) {
+    protected void convert(MultipleViewHolder holder, final MultipleItemEntity entity) {
         super.convert(holder, entity);
         switch (holder.getItemViewType()) {
             case ShopCartItemType.SHOP_CART_ITEM:
@@ -48,6 +51,7 @@ public class ShopCartAdapter extends MultipleRecyclerAdapter {
                 final String desc = entity.getField(ShopCartItemFields.DESC);
                 final int count = entity.getField(ShopCartItemFields.COUNT);
                 final double price = entity.getField(ShopCartItemFields.PRICE);
+                final boolean isSelected = entity.getField(ShopCartItemFields.IS_SELECTED);
                 //取出所有控件
                 final AppCompatImageView imgThumb = holder.getView(R.id.image_item_shop_cart);
                 final AppCompatTextView tvTitle = holder.getView(R.id.tv_item_shop_cart_title);
@@ -56,6 +60,7 @@ public class ShopCartAdapter extends MultipleRecyclerAdapter {
                 final IconTextView iconMinus = holder.getView(R.id.icon_item_minus);
                 final IconTextView iconPlus = holder.getView(R.id.icon_item_plus);
                 final AppCompatTextView tvCount = holder.getView(R.id.tv_item_shop_cart_count);
+                final IconTextView iconIsSelected = holder.getView(R.id.icon_item_shop_cart);
                 //赋值
                 tvTitle.setText(title);
                 tvDesc.setText(desc);
@@ -65,6 +70,29 @@ public class ShopCartAdapter extends MultipleRecyclerAdapter {
                         .load(thumb)
                         .apply(OPTIONS)
                         .into(imgThumb);
+
+                //根据数据状态显示左侧勾勾
+                if (isSelected) {
+                    iconIsSelected.setTextColor(ContextCompat.getColor(Latte.getApplicationContext(), R.color.app_main));
+                } else {
+                    iconIsSelected.setTextColor(Color.GRAY);
+                }
+                //添加左侧勾勾点击事件
+                iconIsSelected.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final boolean currentSelected = entity.getField(ShopCartItemFields.IS_SELECTED);
+                        if (currentSelected) {
+                            iconIsSelected.setTextColor(Color.GRAY);
+                            entity.setField(ShopCartItemFields.IS_SELECTED, false);
+                        } else {
+                            iconIsSelected.setTextColor(ContextCompat.getColor(Latte.getApplicationContext(), R.color.app_main));
+                            entity.setField(ShopCartItemFields.IS_SELECTED, true);
+                        }
+
+                    }
+                });
+
 
                 break;
             default:
--- latte-ec/src/main/java/com/dianbin/latte/ec/main/cart/ShopCartDelegate.java
+++ latte-ec/src/main/java/com/dianbin/latte/ec/main/cart/ShopCartDelegate.java
@@ -1,22 +1,28 @@
 package com.dianbin.latte.ec.main.cart;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Toast;
 
+import com.dianbin.latte.app.Latte;
 import com.dianbin.latte.delegates.bottom.BottomItemDelegate;
 import com.dianbin.latte.ec.R;
 import com.dianbin.latte.ec.R2;
 import com.dianbin.latte.net.RestClient;
 import com.dianbin.latte.net.callBack.ISuccess;
 import com.dianbin.latte.ui.recycle.MultipleItemEntity;
+import com.joanzapata.iconify.widget.IconTextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * Created by zhouyixin on 2017/12/31.
@@ -24,11 +30,73 @@ import butterknife.BindView;
 
 public class ShopCartDelegate extends BottomItemDelegate implements ISuccess {
 
+    private ShopCartAdapter mAdapter = null;
+
     //TODO 底层的封装再看一遍，尤其是ButtonKnife的封装
     @BindView(R2.id.rv_shop_cart)
     RecyclerView mRecyclerView = null;
+    @BindView(R2.id.icon_shop_cart_select_all)
+    IconTextView mIconSelectAll = null;
 
-    private ShopCartAdapter mAdapter;
+    /**
+     * 这部分和视频里的不一样，视频里的代码虽然效率稍微高一点（不需要for循环），但是它容易出错。
+     * 视频中是在Adapter中设个是否被全选的参数，通过public方法传入，在convert方法里置入IS_SELECTED
+     * 这样如果当其它地方有重新调用adapter的notify系列的方法，会出错！即假如这个“是否被全选的参数”
+     * 为false，并且已有一个被选了，此时如果其它地方有调用adapter的notify系列的方法，会出错！
+     */
+    @OnClick(R2.id.icon_shop_cart_select_all)
+    void onClickSelectAll() {
+        final List<MultipleItemEntity> data = mAdapter.getData();
+        final int tag = (int) mIconSelectAll.getTag();
+        if (tag == 0) {
+            for (MultipleItemEntity entity : data) {
+                entity.setField(ShopCartItemFields.IS_SELECTED, true);
+            }
+            mIconSelectAll.setTextColor(ContextCompat.getColor(Latte.getApplicationContext(), R.color.app_main));
+            mIconSelectAll.setTag(1);
+        } else {
+            for (MultipleItemEntity entity : data) {
+                entity.setField(ShopCartItemFields.IS_SELECTED, false);
+            }
+            mIconSelectAll.setTextColor(Color.GRAY);
+            mIconSelectAll.setTag(0);
+        }
+        mAdapter.notifyItemRangeChanged(0, mAdapter.getItemCount());
+    }
+
+    /**
+     * 原视频上的方法是错的，优化了群上的解决方法，更易懂，并且效率更高
+     */
+    @OnClick(R2.id.tv_top_shop_cart_remove_selected)
+    void onClickRemoveSelectedItem() {
+        final List<MultipleItemEntity> data = mAdapter.getData();
+        //要删除的数据
+        final List<MultipleItemEntity> deleteEntities = new ArrayList<>();
+        for (MultipleItemEntity entity : data) {
+            final boolean isSelected = entity.getField(ShopCartItemFields.IS_SELECTED);
+            if (isSelected) {
+                deleteEntities.add(entity);
+            }
+        }
+
+        //从List中最后一个开始删除，防止引起下标的变动
+        for (int i = deleteEntities.size() - 1; i >= 0; i--) {
+            int removePosition = deleteEntities.get(i).getField(ShopCartItemFields.POSITION);
+            mAdapter.remove(removePosition);
+        }
+
+        //不用再取一遍，因为list只是引用，只有一个对象
+        final int size = data.size();
+        for (int j = 0; j < size; j++) {
+            data.get(j).setField(ShopCartItemFields.POSITION, j);
+        }
+    }
+
+    @OnClick(R2.id.tv_top_shop_cart_clear)
+    void onClickClear(){
+        mAdapter.getData().clear();
+        mAdapter.notifyDataSetChanged();
+    }
 
     @Override
     public Object setLayout() {
@@ -37,7 +105,7 @@ public class ShopCartDelegate extends BottomItemDelegate implements ISuccess {
 
     @Override
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
-
+        mIconSelectAll.setTag(0);
     }
 
     @Override
